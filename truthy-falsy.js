f9fcdd2215317f0d35e1dca9026d01b8cacf2c03
Enzo Nakahara <japanakah@outlook.com>
2022-07-26 22:14:55 -0300

    first commit

---
+++ truthy-falsy.js
@@ -0,0 +1,19 @@
+// boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+// 0 = false
+// 1 = true
+
+// null 
+// undefined
+
+let minhaVariavel;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof minhaVariavel)
+console.log(typeof varNull)
