b29ccc7be97c03bd1f427fae544ec99e46fe2d5b
Enzo Nakahara <japanakah@outlook.com>
2022-08-01 23:53:46 -0300

    curso completed, now i stay in array js

--- arrow-function.js
+++ arrow-function.js
@@ -14,3 +14,15 @@ const somaNumerosPequenos = (num1, num2) => {
         return num1 + num2;
     }
 }
+
+let num = 15
+
+if (num >= 0 && num <= 10) {
+    console.log('número entre zero e dez');
+  } else if (num > 10 && num <= 20) {
+    console.log('número entre dez e vinte');
+  } else if (num > 20 && num <= 30) {
+    console.log('número entre vinte e trinta');
+  } else {
+    console.log('outro número');
+  }
