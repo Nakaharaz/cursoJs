ac814bc8b1e9f534fdb00dd5bd25768e9248e0f9
Enzo Nakahara <japanakah@outlook.com>
2022-08-11 19:20:23 -0300

    algorithm class 2

--- js-algorithm/index.js
+++ js-algorithm/index.js
@@ -1,12 +1,11 @@
-const precosLivros = [25, 15, 30, 50, 45, 20];
+const livros = require('./listaLivros');
 
 let maisBarato = 0;
 
-for(let atual = 0; atual < precosLivros.length; atual++) {
-    //código 
-    if(precosLivros[atual] < precosLivros[maisBarato]) {
+for(let atual = 0; atual < livros.length; atual++) {
+    if(livros[atual].preco < livros[maisBarato].preco) {
         maisBarato = atual;
     }
 }
 
-console.log(`O livro mais barato é ${precosLivros[maisBarato]}`)
+console.log(`O livro mais barato é ${livros[maisBarato].preco} e o título é ${livros[maisBarato].titulo}`)
