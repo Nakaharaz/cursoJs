8d1419350b5e34584e575e77b975600326dcbf50
Enzo Nakahara <japanakah@outlook.com>
2022-08-13 11:11:16 -0300

    cap 2 finalizated

--- js-algorithm/selectionSort.js
+++ js-algorithm/selectionSort.js
@@ -3,13 +3,10 @@ const menorValor = require('./menorValor')
 
 for (let atual = 0; atual < livros.length - 1; atual++) {
     let menor = menorValor(livros, atual);
-    
-    console.log(atual)
-    let livroAtual = livros[atual];
-    console.log(livroAtual)
+
+    let livroAtual = livros[atual];    
     let livroMenorPreco = livros[menor];
-    console.log(livroMenorPreco)
-    
+
     livros[atual] = livroMenorPreco
     livros[menor] = livroAtual
 }
