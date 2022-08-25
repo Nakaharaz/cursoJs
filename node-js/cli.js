9b59423c956814c7dfe3a97eb6737b99a2df2a94
Enzo Nakahara <japanakah@outlook.com>
2022-08-25 19:08:10 -0300

    merge casa oneck

--- node-js/cli.js
+++ node-js/cli.js
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-const chalk = require('chalk')
-const pegaArquivo = require('./index');
-
-const caminho = process.argv;
-
-async function processaTexto (caminhoDoArquivo) {
-    const resultado = await pegaArquivo(caminhoDoArquivo[2]);
-    console.log(chalk.yellow('Lista de links'), resultado)
-}
-
-processaTexto(caminho)
-=======
 const chalk = require('chalk');
 const pegaArquivo = require('./index');
 const validaURLs = require('./http-validacao');
@@ -29,4 +16,3 @@ async function processaTexto(caminhoDeArquivo) {
 }
 
 processaTexto(caminho);
->>>>>>> 7a5c3e8480b49a2fbfbc6f7f563cbb73c25ed2c3
