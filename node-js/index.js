71f27a47f1c40efe2126e92d8b7c6f2cf6513887
Enzo Nakahara <japanakah@outlook.com>
2022-08-25 19:05:23 -0300

    git casa

--- node-js/index.js
+++ node-js/index.js
@@ -1,27 +1,34 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\(https?:\/\/([^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+
+    while((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push({[ temp[1]]: temp[2]})
+    }
+    return arrayResultados === 0 ? 'Não existem links' : arrayResultados;
+}
+
 function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no caminho'));
 }
 
-function pegaArquivo (caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.promises.readFile(caminhoDoArquivo, encoding)
-    .then((texto) => chalk.green(console.log(texto)))
-    .catch((erro) => trataErro(erro))
+async function pegaArquivo (caminhoDoArquivo) {
+    const encoding = 'utf-8' 
+    
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        return extraiLinks(texto);
+    } catch(erro) {
+        trataErro(erro)
+    } finally {
+        console.log(chalk.yellow('A operação foi concluida'))
+    }
+    
 }
 
-
-/* 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro) {
-            trataErro(erro);
-        }
-        console.log(chalk.green(texto));
-    })
-} */
-
-pegaArquivo('./arquivos/texto1.md')
+module.exports = pegaArquivo;
